a04d8d57cf1f5028208f2409d6cbb58ce675b625
XiaoLFeng <gm@x-lf.cn>
2024-06-10 20:21:01 +0800

    feat(拦截器): 更新为获取 ctx 内容

--- bresult/result_response.go
+++ bresult/result_response.go
@@ -1,6 +1,7 @@
 package bresult
 
 import (
+	"context"
 	"github.com/bamboo-services/bamboo-utils/bcode"
 	"github.com/bamboo-services/bamboo-utils/btype"
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -13,11 +14,12 @@ import (
 // 用于写入响应信息, 用于返回错误码信息.
 //
 // # 参数
-//   - r			http 信息(*ghttp.Request)
+//   - ctx			请求的上下文(*context.Context)
 //   - getCode		错误码(BCode)
 //   - msg			错误信息(string)
 //   - res			数据(interface)
-func WriteResponse(r *ghttp.Request, getCode bcode.BCode, msg string, res interface{}) {
+func WriteResponse(ctx context.Context, getCode bcode.BCode, msg string, res interface{}) {
+	r := ghttp.RequestFromCtx(ctx)
 	r.Response.WriteJson(btype.BaseResponse{
 		Output:       getCode.Output(),
 		Code:         getCode.Code(),
