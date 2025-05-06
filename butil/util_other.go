d55b0cf800d25372162ec8c69a0596d323a56c27
XiaoLFeng <gm@x-lf.cn>
2025-05-06 12:44:17 +0800

    refactor(docs): 优化方法注释格式及描述
    调整了方法注释，统一格式，简化表述，提升可读性和准确性，明确参数和返回值的说明。

--- butil/util_other.go
+++ butil/util_other.go
@@ -8,32 +8,24 @@ import (
 	"strings"
 )
 
-// TokenRemoveBearer
+// TokenRemoveBearer 移除字符串中的 "Bearer " 前缀并返回处理后的结果。
 //
-// # 移除Token中的Bearer
+// 参数:
+//   - getToken 	要处理的字符串(string)
 //
-// 用于移除 Token 中的 "Bearer " 前缀，返回移除后, 前缀的 Token.
-//
-// # 参数:
-//   - getToken 	带有 "Bearer " 前缀的 Token(string)
-//
-// # 返回:
-//   - string 	去除 "Bearer " 前缀后的 Token
+// 返回:
+//   - string 	处理后的结果
 func TokenRemoveBearer(getToken string) string {
 	return strings.Replace(getToken, "Bearer ", "", 1)
 }
 
-// RandomString
+// RandomString 生成指定长度的随机字符串，由字母和数字组成。
 //
-// # 生成随机字符串
+// 参数:
+//   - length: 要生成的字符串长度
 //
-// 用于生成一个指定长度的随机字符串，返回生成的随机字符串. 生成的字符串为 string 类型.
-//
-// # 参数:
-//   - length 	生成的字符串长度(int)
-//
-// # 返回:
-//   - string 	生成的随机字符串
+// 返回:
+//   - 包含随机字符的字符串
 func RandomString(length int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	result := make([]byte, length)
@@ -44,19 +36,13 @@ func RandomString(length int) string {
 	return string(result)
 }
 
-// GetVisitorProtocol
+// GetVisitorProtocol 根据上下文获取访问协议标识。
 //
-// # 获取访问者协议
+// 参数:
+//   - ctx: 请求的上下文对象。
 //
-// 用于获取访问者使用的协议类型，返回协议类型. 返回的协议类型为 string 类型. 如果是 SSL 协议则返回 "SSL"，否则返回 "TLS".
-// 即若请求中访问协议为 HTTPS 则返回 "SSL"，否则返回 "TLS".
-// 若 context.Context 为空则返回空字符串.
-//
-// # 参数:
-//   - ctx 	请求的上下文(context.Context)
-//
-// # 返回:
-//   - string 	协议类型
+// 返回:
+//   - 返回访问协议的标识，如 "SSL" 或 "TLS"。若请求对象为空，则返回空字符串。
 func GetVisitorProtocol(ctx context.Context) string {
 	getRequest := ghttp.RequestFromCtx(ctx)
 	if getRequest != nil {
